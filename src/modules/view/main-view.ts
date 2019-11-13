e2fed1c477171b43792b4c93bd6479073f91c573
Geani Pocroianu <pocroianu.nicolae@gmail.com>
2019-11-13 20:59:06 +0200

    chg: dev: Refactoring

--- src/modules/view/main-view.ts
+++ src/modules/view/main-view.ts
@@ -6,6 +6,7 @@ import View = puremvc.View;
 import Container = PIXI.Container;
 import WebGLRenderer = PIXI.WebGLRenderer;
 import CanvasRenderer = PIXI.CanvasRenderer;
+import {Parameters} from "../static/parameters";
 
 export class MainView extends View {
 
@@ -14,7 +15,12 @@ export class MainView extends View {
 
     initializeView(): void {
         super.initializeView();
-        this.createApp();
+        this._pixiRenderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, Parameters.PIXI_APPLICATION_SETTINGS);
+        document.body.appendChild(this._pixiRenderer.view);
+
+        // create the root of the scene graph
+        this._pixiStage = new PIXI.Container();
+        this.addGraphics();
     }
 
     /**
@@ -29,34 +35,26 @@ export class MainView extends View {
     }
 
 
-    createApp(): void {
-        var renderer = PIXI.autoDetectRenderer(800, 600, { antialias: true });
-        document.body.appendChild(renderer.view);
+    addGraphics(): void {
+        let graphics = new PIXI.Graphics();
 
-        // create the root of the scene graph
-        var stage = new PIXI.Container();
-
-        stage.interactive = true;
-
-        var graphics = new PIXI.Graphics();
-
-// set a fill and line style
+        // set a fill and line style
         graphics.beginFill(0xFF3300);
         graphics.lineStyle(4, 0xffd900, 1);
 
-// draw a shape
-        graphics.moveTo(50,50);
+        // draw a shape
+        graphics.moveTo(50, 50);
         graphics.lineTo(250, 50);
         graphics.lineTo(100, 100);
         graphics.lineTo(50, 50);
         graphics.endFill();
 
-// set a fill and a line style again and draw a rectangle
+        // set a fill and a line style again and draw a rectangle
         graphics.lineStyle(2, 0x0000FF, 1);
         graphics.beginFill(0xFF700B, 1);
         graphics.drawRect(50, 250, 120, 120);
 
-// draw a rounded rectangle
+        // draw a rounded rectangle
         graphics.lineStyle(2, 0xFF00FF, 1);
         graphics.beginFill(0xFF00BB, 0.25);
         graphics.drawRoundedRect(150, 450, 300, 100, 15);
@@ -65,20 +63,20 @@ export class MainView extends View {
         // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
         graphics.lineStyle(0);
         graphics.beginFill(0xFFFF0B, 0.5);
-        graphics.drawCircle(470, 90,60);
+        graphics.drawCircle(470, 90, 60);
         graphics.endFill();
 
 
-        stage.addChild(graphics);
+        this._pixiStage.addChild(graphics);
 
-// run the render loop
+        let animate = () => {
+            this._pixiRenderer.render(this._pixiStage);
+            requestAnimationFrame(animate);
+        };
+
+        // run the render loop
         animate();
 
-        function animate() {
-
-            renderer.render(stage);
-            requestAnimationFrame( animate );
-        }
     }
 
 
